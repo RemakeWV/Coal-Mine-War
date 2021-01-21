2180a3632c2fb465f1f325076d8a7b4301f8985f
CoryBrown-42 <yroc.999@gmail.com>
2021-01-21 02:36:19 -0500

    LODs

--- Python/init_unreal.py
+++ Python/init_unreal.py
@@ -1,10 +1,12 @@
 import unreal
 import sys
 
-unreal.log_warning("Hello Unreal!")
+
 
 file = open("C:\Users\Cory\Desktop\Mine Wars Museum\Python\Test.txt", "a+")
-
 file.write(sys.argv[1])
+
+unreal.log_warning("Log has been updated. Opening README for change dev logs")
+os.startfile("C:\Users\Cory\Desktop\Mine Wars Museum\README.md")
 
 file.close()
--- Python/logger.py
+++
@@ -1,4 +0,0 @@
-import unreal
-import os
-
-os.startfile("C:\Users\Cory\Desktop\Mine Wars Museum\README.md")
